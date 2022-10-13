398add6800171b6e16531f9f18b9ede436cd89a8
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2022-10-13 21:43:02 +0800

    lots of changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from '@mui/material/button';
-import Navbar from './navbar';
+import './App.scss';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout';
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Button variant='contained'>Hello World</Button>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+    </Routes>
   );
 }
 
